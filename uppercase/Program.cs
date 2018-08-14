06445e204287762c6d2a2a3510fe0367fa3ca21b
wambui7 <shiko.sheila@gmail.com>
2018-08-14 18:16:53 -0500

    no changes

--- uppercase/Program.cs
+++ uppercase/Program.cs
@@ -5,7 +5,9 @@ namespace uppercase
     class Program
     {
         static void Main(string[] args)
+
         {
+            
             string mystring = "I am eating dinner now";
             mystring = mystring.ToUpper();
             Console.WriteLine(mystring);
